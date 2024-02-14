ee5ba28e518ac8a0d8ccffac2f497d4908daa7b9
DGD411 <91732632+DGD411@users.noreply.github.com>
2024-02-14 21:37:26 +0100

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -2,26 +2,52 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-public class Client {
+public class Server {
     public static void main(String[] args) {
         try {
-            Socket clientSocket = new Socket("localhost", 12345);
+            ServerSocket serverSocket = new ServerSocket(12345);
+            System.out.println("Servidor iniciat. Esperant clients...");
 
-            ObjectOutputStream outToServer = new ObjectOutputStream(clientSocket.getOutputStream());
-            ObjectInputStream inFromServer = new ObjectInputStream(clientSocket.getInputStream());
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Client connectat.");
 
-            List<Integer> numberList = Arrays.asList(3, 1, 4, 1, 5, 9, 2, 6, 5, 3);
-            Llista llista = new Llista("Exemple", numberList);
-
-            outToServer.writeObject(llista);
-            System.out.println("Llista enviada al servidor: " + llista.getNom() + " - " + llista.getNumberList());
-
-            Llista modifiedList = (Llista) inFromServer.readObject();
-            System.out.println("Llista rebuda del servidor: " + modifiedList.getNom() + " - " + modifiedList.getNumberList());
-
-            clientSocket.close();
-        } catch (IOException | ClassNotFoundException e) {
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                clientHandler.start();
+            }
+        } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static class ClientHandler extends Thread {
+        private final Socket clientSocket;
+
+        public ClientHandler(Socket socket) {
+            this.clientSocket = socket;
+        }
+
+        public void run() {
+            try {
+
+                ObjectInputStream inFromClient = new ObjectInputStream(clientSocket.getInputStream());
+                ObjectOutputStream outToClient = new ObjectOutputStream(clientSocket.getOutputStream());
+
+                Llista receivedList = (Llista) inFromClient.readObject();
+                System.out.println("Llista rebuda del client: " + receivedList.getNom() + " - " + receivedList.getNumberList());
+
+                Set<Integer> uniqueNumbers = new TreeSet<>(receivedList.getNumberList());
+                List<Integer> sortedList = new ArrayList<>(uniqueNumbers);
+
+                Llista modifiedList = new Llista(receivedList.getNom(), sortedList);
+                outToClient.writeObject(modifiedList);
+                System.out.println("Llista modificada enviada al client: " + modifiedList.getNom() + " - " + modifiedList.getNumberList());
+
+
+                clientSocket.close();
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
